70daab3035e9d92d86a7ac01243eec17cd038e1d
Jip Rietveld <jip_rietveld@hotmail.com>
2022-11-21 20:57:54 +0100

    Convert list of dictionaries to a DataFrame

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import requests
+import pandas as pd
 
 
 def load_price_data():
     query = """query MarketPrices {
     marketPricesElectricity(startDate: "2021-11-21", endDate: "2021-11-22") {
-        till
         from
+        till
         marketPrice    
     }
     marketPricesGas(startDate: "2021-11-21", endDate: "2021-11-22") {
-        from
         till
+        from
         marketPrice
     }
     }"""
     headers = {'content-type': 'application/json'}
     response = requests.post('https://graphcdn.frankenergie.nl', json={'query': query}, headers=headers)
     data = response.json()
-    print(data)
+    df = pd.DataFrame(data['data']['marketPricesElectricity'])
+    df['from'] = pd.to_datetime(df['from'], utc=True)
+    df['till'] = pd.to_datetime(df['till'], utc=True)
+    print(df)
 
 
 if __name__ == '__main__':
