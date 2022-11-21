09f87b987f3391758d6418025cccd784410ab4f0
Jip Rietveld <jip_rietveld@hotmail.com>
2022-11-21 21:04:15 +0100

    Add day parameter

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import requests
 import pandas as pd
+import datetime as dt
 
 
-def load_price_data():
-    query = """query MarketPrices {
-    marketPricesElectricity(startDate: "2021-11-21", endDate: "2021-11-22") {
+def load_price_data(day: dt.datetime | dt.date = None):
+    if day is None:
+        day = dt.date.today() + dt.timedelta(days=1)
+    day_str = day.strftime('%Y-%m-%d')
+
+    query = f"""query MarketPrices {{
+    marketPricesElectricity(startDate: "{day_str}", endDate: "{day_str}") {{
         from
         till
         marketPrice    
-    }
-    marketPricesGas(startDate: "2021-11-21", endDate: "2021-11-22") {
+    }}
+    marketPricesGas(startDate: "{day_str}", endDate: "{day_str}") {{
         till
         from
         marketPrice
-    }
-    }"""
+    }}
+    }}"""
     headers = {'content-type': 'application/json'}
     response = requests.post('https://graphcdn.frankenergie.nl', json={'query': query}, headers=headers)
     data = response.json()
@@ -25,4 +30,4 @@ def load_price_data():
 
 
 if __name__ == '__main__':
-    load_price_data()
+    load_price_data(dt.date(2022, 11, 20))
